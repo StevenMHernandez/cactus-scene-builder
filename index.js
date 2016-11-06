3d927078a5e2db4242e77d85ffa2150f1f433376
StevenmHernandez <contact@stevenhdesigns.com>
2016-11-06 12:28:50 -0500

    added functionality for buttons, began organizing code into functions

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
+    $(window).on('resize', function(){
+        setupCanvas();
+    });
+
     var data = {
         operators: {
-            operator1: {
+            operator_1: {
                 top: 20,
                 left: 20,
                 properties: {
-                    inputs: {},
+                    inputs: {
+                        input_1: {
+                            label: '(:i)'
+                        }
+                    },
                     outputs: {
-                        outs: {
-                            label: '(:i)',
-                            multiple: true
+                        output_1: {
+                            label: '(:i)'
                         }
                     }
                 }
@@ -19,7 +26,7 @@ $(document).ready(function () {
 
     var $flowchart = $('#flow_chart');
 
-    $flowchart.flowchart({
+    var test = $flowchart.flowchart({
         data: data,
         onOperatorSelect: function (operatorId) {
             var operator = $flowchart.flowchart('getOperatorData', operatorId);
@@ -34,67 +41,125 @@ $(document).ready(function () {
         }
     });
 
+    /////////
+    // FLOWCHART HELPERS
+
+    function getOperatorData(operatorId) {
+        return $flowchart.flowchart('getOperatorData', operatorId);
+    }
+
+    var operatorI = 1;
+
+    function buildNewOperator() {
+        console.log(getFlowChartData());
+        var prevOperator = getOperatorData("operator_" + operatorI);
+
+        operatorI++;
+
+        var newL = prevOperator.left + 150 + 150 < $flowchart.width() ? prevOperator.left + 150 : 20;
+        var newT = newL == 20 ? prevOperator.top + 50 : prevOperator.top;
+
+        var operatorId = 'operator_' + operatorI;
+        var operatorData = {
+            top: newT,
+            left: newL,
+            properties: {
+                inputs: {
+                    input_1: {
+                        label: '(:i)'
+                    }
+                },
+                outputs: {
+                    output_1: {
+                        label: '(:i)'
+                    }
+                }
+            }
+        };
+
+        var linkData = {
+            fromConnector: "output_1",
+            toConnector: "input_1",
+            fromOperator: "operator_" + (operatorI - 1),
+            toOperator: "operator_" + operatorI
+        };
+
+        $flowchart.flowchart('createOperator', operatorId, operatorData);
+        $flowchart.flowchart('createLink', operatorId, linkData);
+    }
+
+    function getFlowChartData() {
+        return $flowchart.flowchart('getData');
+    }
+
+    /////////
+    // CONTROLS
+
+    $("#new_frame").on("click", function () {
+        buildNewOperator();
+    });
+
+    $("#reverse_frame").on("click", function () {
+        // TODO
+    });
+
+    $("#clear_frame").on("click", function () {
+        // TODO
+    });
 
     /////////
     // CANVAS
 
     var canvas = document.getElementById("canvas");
-
-    canvas.width = window.innerWidth - canvas.offsetLeft;
-    canvas.height = window.innerHeight - canvas.offsetTop;
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     var mouseClicked = false;
-    //var color = "white";
-
-    $("#canvas").on("mousemove", function (e) {
-        if (mouseClicked) {
-            draw(e);
-        }
-    }).on("mousedown", function (e) {
-        mouseClicked = true;
-
-        var x = (e.clientX - canvas.offsetLeft) / (window.innerWidth - canvas.offsetLeft) * canvas.width;
-        var y = (e.clientY - canvas.offsetTop) / (window.innerHeight - canvas.offsetTop) * canvas.height;
-
-        var color = ctx.getImageData(x, y, 1, 1).data;
-
-        ctx.fillStyle = color[0] < 128 ? "white" : "black";
-
-        draw(e);
-    }).on("mouseup", function () {
-        mouseClicked = false;
-
-        // TODO: store image data
-    });
-
-    var screen_image_height = 32;
 
     var maxW, maxH, sz;
 
-    var widthToHeightRatio = 1; // normally 2, but we will only see the middle square
+    var screen_image_height = 32;
 
-    if (canvas.width / widthToHeightRatio > canvas.height) {
-        sz = canvas.height / screen_image_height;
-    } else {
-        sz = canvas.width / (screen_image_height * widthToHeightRatio);
+    setupCanvas();
+
+    function setupCanvas() {
+        setCanvasSize();
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        loadGrid();
     }
 
-    sz = Math.floor(sz);
+    function setCanvasSize() {
+        editBox = document.getElementById("edit");
+        $editBox = $("#edit");
 
-    maxH = sz * screen_image_height;
-    maxW = maxH * widthToHeightRatio;
+        canvas.width = window.innerWidth - canvas.offsetLeft;
+        canvas.height = window.innerHeight - canvas.offsetTop;
+
+        var widthToHeightRatio = 1; // normally 2, but we will only see the middle square
+
+        if ($editBox.width() / widthToHeightRatio > $editBox.height()) {
+            sz = $editBox.height() / screen_image_height;
+        } else {
+            sz = $editBox.width() / (screen_image_height * widthToHeightRatio);
+        }
+        sz = Math.floor(sz);
+
+        maxH = sz * screen_image_height;
+        maxW = maxH * widthToHeightRatio;
+
+        canvas.width = maxW;
+        canvas.height = maxH;
+
+        canvas.style.marginLeft = (($editBox.width() - canvas.width) / 2) + "px";
+        canvas.style.marginTop = (($editBox.height() - canvas.height) / 2) + "px";
+    }
 
     function draw(e) {
-        var x = (e.clientX - canvas.offsetLeft) / (window.innerWidth - canvas.offsetLeft) * canvas.width;
-        var y = (e.clientY - canvas.offsetTop) / (window.innerHeight - canvas.offsetTop) * canvas.height;
+        var coords = getXY(e);
 
-        ctx.fillRect(Math.floor(x / sz) * sz, Math.floor(y / sz) * sz, sz - 2, sz - 2);
+        ctx.fillRect(Math.floor(coords.x / sz) * sz, Math.floor(coords.y / sz) * sz, sz - 2, sz - 2);
     }
-
-    drawGrid();
 
     function drawGrid() {
         ctx.strokeStyle = '#999';
@@ -113,5 +178,36 @@ $(document).ready(function () {
             ctx.stroke();
         }
     }
+
+    function loadGrid() {
+        drawGrid();
+    }
+
+    function getXY(e) {
+        return {
+            x: e.clientX - canvas.offsetLeft,
+            y: e.clientY - canvas.offsetTop
+        }
+    }
+
+    $("#canvas").on("mousemove", function (e) {
+        if (mouseClicked) {
+            draw(e);
+        }
+    }).on("mousedown", function (e) {
+        mouseClicked = true;
+
+        var coords = getXY(e);
+
+        var color = ctx.getImageData(coords.x, coords.y, 1, 1).data;
+
+        ctx.fillStyle = color[0] < 128 ? "white" : "black";
+
+        draw(e);
+    }).on("mouseup", function () {
+        mouseClicked = false;
+
+        // TODO: store image data
+    });
 });
 
